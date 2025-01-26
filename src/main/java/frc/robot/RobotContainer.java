ee1ce1adc66195a3175187a6a6aa6cca55e962ce
RehanNazeem <rehannazeem8@gmail.com>
2025-01-26 11:41:24 -0800

    1/26 pt1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,28 +2,52 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.PS4Controller;
 import frc.robot.commands.SwerveDriveCommand;
+import frc.robot.subsystems.AlgaeHolderSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class RobotContainer {
-    private final DriveSubsystem driveSubsystem;
-    private final PS4Controller ps5Controller;
+
+    // Controllers
+    private final PS4Controller ps5Controller = new PS4Controller(0);
+
+    // Subsystems
+    private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+    private final AlgaeHolderSubsystem algaeHolderSubsystem = new AlgaeHolderSubsystem();
+    private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+    private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+
+    // Commands
+    private final SwerveDriveCommand swerveDriveCommand = 
+        new SwerveDriveCommand(driveSubsystem, ps5Controller);
 
     public RobotContainer() {
-        // Initialize subsystems
-        driveSubsystem = new DriveSubsystem();
-
-        // Initialize the PS4 controller on port 0
-        ps5Controller = new PS4Controller(0);
-
         // Configure default commands
-        driveSubsystem.setDefaultCommand(new SwerveDriveCommand(driveSubsystem, ps5Controller));
+        configureDefaultCommands();
+
+        // Configure button bindings
+        configureButtonBindings();
+    }
+
+    private void configureDefaultCommands() {
+        
+        driveSubsystem.setDefaultCommand(swerveDriveCommand);
+
+    }
+
+    private void configureButtonBindings() {
+       
+    }
+
+    public Command getAutonomousCommand() {   
+        return null; 
     }
 }
