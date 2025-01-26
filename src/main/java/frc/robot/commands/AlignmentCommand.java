dc8743b1a044380bedcb7ef8741bda64d9e60d74
Damien Boisvert <damien@alphagame.dev>
2025-01-26 13:25:55 -0800

    working on debugging :/

--- src/main/java/frc/robot/commands/AlignmentCommand.java
+++ src/main/java/frc/robot/commands/AlignmentCommand.java
@@ -1,9 +1,9 @@
-@ -0,0 +1,41 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.*;
+
 import frc.robot.subsystems.DriveSubsystem;
-import frc.ControlInput;
+import frc.robot.ControlInput;
 
 public class AlignmentCommand extends CommandBase {
     private DriveSubsystem driveSubsystem;
